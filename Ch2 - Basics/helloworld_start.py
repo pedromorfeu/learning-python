5a7e1446ea40e3e5c8a2ab2429a74c9b3240493c
Pedro Mano <pedro.mano@adevinta.com>
2024-10-07 16:13:39 +0100

    initial push

--- Ch2 - Basics/helloworld_start.py
+++ Ch2 - Basics/helloworld_start.py
@@ -3,4 +3,11 @@
 # LinkedIn Learning Python course by Joe Marini
 #
 
+def main():
+    print("hello world")
+    name = input("what's your name? ")
+    print("nice to meet you,", name)
 
+
+if __name__ == "__main__":
+    main()
